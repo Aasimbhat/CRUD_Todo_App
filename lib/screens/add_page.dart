3e573093fd2dd904524c63fd72cdf1c505fc9637
Kamran-7 <90784719+Aasimbhat@users.noreply.github.com>
2023-03-05 16:35:23 +0530

    delete and post api integrated

--- lib/screens/add_page.dart
+++ lib/screens/add_page.dart
@@ -14,67 +14,138 @@ class _AddTodoPageState extends State<AddTodoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFF73be93),
       appBar: AppBar(
+        backgroundColor: Color(0xFF192c49),
         title: Center(child: Text("Add Todo")),
       ),
       body: ListView(
         padding: const EdgeInsets.all(20),
         children: [
-          TextField(
-            controller: titleController,
-            decoration: InputDecoration(hintText: "Title"),
-          ),
-          TextField(
-            controller: descriptionController,
-            decoration: InputDecoration(
-              hintText: "Description",
+          Container(
+            padding: const EdgeInsets.all(12),
+            height: 100,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Color(0xFF192c49)),
+            child: TextField(
+              controller: titleController,
+              decoration: InputDecoration(
+                  hintText: "Title",
+                  hintStyle: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20)),
             ),
-            keyboardType: TextInputType.multiline,
-            minLines: 5,
-            maxLines: 8,
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Container(
+            padding: const EdgeInsets.all(12),
+            height: 200,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Color(0xFF192c49)),
+            child: TextField(
+              controller: descriptionController,
+              decoration: InputDecoration(
+                hintText: "Description",
+                 hintStyle: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20)
+              ),
+              keyboardType: TextInputType.multiline,
+              minLines: 5,
+              maxLines: 8,
+            ),
           ),
           SizedBox(
             height: 20,
           ),
-          ElevatedButton(onPressed: () {
-            submitData();
-          }, child: Text("Submit"))
+          InkWell(
+            onTap: (){
+              submitData();
+            },
+            child: Container(
+              height: 50,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Color(0xFF192c49),
+                
+              ),
+              child: Center(
+                child: Text("Submit",
+                style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
+                ),
+              ),
+            ),
+          ),
+          // ElevatedButton(
+            
+          //     onPressed: () {
+          //       submitData();
+          //     },
+          //     child: Text("Submit"))
         ],
       ),
     );
   }
 
-  Future <void> submitData() async{
+  Future<void> submitData() async {
     final title = titleController.text;
     final description = descriptionController.text;
     final body = {
-      "title": title, "description": description, "is_completed": false,
-
+      "title": title,
+      "description": description,
+      "is_completed": false,
     };
-    final url ="https://api.nstack.in/v1/todos";
-    final uri=Uri.parse(url);
-    final response =await http.post(uri,body:jsonEncode(body),
-    headers: {"Content-Type":"application/json"}
-    );
-    if(response.statusCode==201){
-      titleController.text="";
-      descriptionController.text="";
-          showSuccessMessage("Creation Succcess");
-
-    }else{
+    final url = "https://api.nstack.in/v1/todos";
+    final uri = Uri.parse(url);
+    final response = await http.post(uri,
+        body: jsonEncode(body), headers: {"Content-Type": "application/json"});
+    if (response.statusCode == 201) {
+      titleController.text = "";
+      descriptionController.text = "";
+      showSuccessMessage("Creation Succcess");
+    } else {
       showErrorMessage("Creation Failed");
-
     }
- 
-    
   }
-  void showSuccessMessage(String message){
-    final snackBar=SnackBar(content: Text("Creation Success"));
+
+  void showSuccessMessage(String message) {
+    final snackBar = SnackBar(
+      content: Container(
+        height: 50,
+        child: Text(
+          "Creation Success",
+          style: TextStyle(
+              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),
+        ),
+      ),
+      backgroundColor: Color(0xFF192c49),
+    );
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 
-   void showErrorMessage(String message){
-    final snackBar=SnackBar(content: Text("Error"));
+  void showErrorMessage(String message) {
+    final snackBar = SnackBar(
+      content: Text(
+        "Please Type In Something :)",
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      backgroundColor: Colors.redAccent,
+    );
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
--- lib/screens/todo_list.dart
+++ lib/screens/todo_list.dart
@@ -13,42 +13,104 @@ class TodoList extends StatefulWidget {
 
 class _TodoListState extends State<TodoList> {
   List items =[];
+  bool isLoading=true;
   @override
   void initState() {
     fetechTodo();
-    // TODO: implement initState
     super.initState();
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFF73be93),
       appBar: AppBar(
         title: Center(child: Text("Todo List")),
-        backgroundColor: Colors.grey[700],
+        backgroundColor: Color(0xFF192c49),
       ),
       body: RefreshIndicator(
         onRefresh: fetechTodo,
         child: ListView.builder(
           itemCount: items.length,
+          padding: EdgeInsets.all(15),
           itemBuilder: (context , index){
             final item=items[index] as Map;
-          return ListTile(
-            leading: CircleAvatar(child: Text('${index +1}')),
-            title: Text(item['title']),
-            subtitle: Text(item['description']),
+            final id=item['_id'] as String;
+          return Container(
+            height: 100,
+            child: Card(
+              color: Color(0xFF192c49),
+              shadowColor: Colors.white,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+              
+              child: ListTile(
+              
+                leading: CircleAvatar(child: Text('${index +1}',style: TextStyle(fontWeight: FontWeight.bold),),
+                backgroundColor:  Color(0xFF73be93),
+                ),
+                title: Text(item['title']),
+                subtitle: Text(item['description']),
+                trailing: PopupMenuButton(
+                  
+                  onSelected: (value) {
+                    if(value== "edit"){
+                  Get.to(()=>AddTodoPage());
+            
+                    }else if(value=="delete"){
+                      deleteById(id);
+                    }
+                  },
+                  itemBuilder: (context) {
+                    return [
+                      PopupMenuItem(child:Icon(Icons.edit_attributes,size: 30,) ,
+                      value: "Edit",
+                      ),
+                      PopupMenuItem(child:Icon(Icons.delete,size: 30,),
+                      value: 'delete',)
+                    ];
+                  },
+                ),
+            
+              ),
+            ),
           );
         }
         ),
       ),
        floatingActionButton: FloatingActionButton.extended(
+        backgroundColor:Color(0xFF192c49) ,
         onPressed: (){
           Get.to(()=>AddTodoPage());
         },
-         label: Text("Add Todo")),
+         label: Text("Add Todo",
+         style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold
+         ),)),
       
     );
   }
+  
+
+
+
+  Future<void> deleteById(String id)async{
+     final url="https://api.nstack.in/v1/todos/$id";
+     final uri=Uri.parse(url); 
+     final response= await http.delete(uri);
+     if(response.statusCode==200){
+       final filtered  =items.where((element) => element['_id']!=id).toList();  
+       setState(() {
+         items=filtered;
+       }); 
+     }else{
+     showErrorMessage("Unable To Delete");
+     }
+  }
   Future<void> fetechTodo() async{
+    setState(() {
+      
+      isLoading=true;
+    });
     final url ="https://api.nstack.in/v1/todos?page=1&limit=10";
     final uri = Uri.parse(url);
     final response=await http.get(uri);
@@ -58,9 +120,20 @@ class _TodoListState extends State<TodoList> {
             setState(() {
               items=result;
             });
-    }else{
-
     }
-  
+  setState(() {
+    
+    isLoading=false;
+  });
+  }
+
+
+   void showErrorMessage(String message){
+    final snackBar=SnackBar(content: Text("Please Type In Something :)",style: TextStyle(
+      color: Colors.white,
+      fontWeight: FontWeight.bold,
+    ),),
+    backgroundColor: Colors.redAccent,);
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
